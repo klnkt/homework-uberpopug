c8557de5ccafac24ba67a9a5f2aac99e8c9b4542
klnkt <katja@efremova.dev>
2023-08-13 15:37:02 +0200

    Add devise & doorkeeper

--- auth/config/routes.rb
+++ auth/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  use_doorkeeper
+  devise_for :accounts, controllers: {
+    registrations: 'accounts/registrations',
+    sessions: 'accounts/sessions'
+  }
 
   # Defines the root path route ("/")
   # root "articles#index"
