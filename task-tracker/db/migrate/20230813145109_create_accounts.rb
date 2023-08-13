d94628a8813c38fb5594229bec8aa902c37b4659
klnkt <katja@efremova.dev>
2023-08-13 18:25:25 +0200

    Add tasks & accounts tables

---
+++ task-tracker/db/migrate/20230813145109_create_accounts.rb
@@ -0,0 +1,14 @@
+class CreateAccounts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :accounts do |t|
+      t.string :public_id
+      t.string :email
+      t.string :full_name
+      t.string :position
+      t.string :role
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
