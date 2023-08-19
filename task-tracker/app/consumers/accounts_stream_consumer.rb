595940e3cc7d2a70c39851ce841fee7a8728243a
Katja Efremova <kefremova@babbel.com>
2023-08-19 23:22:33 +0200

    Add schema for tasks events

--- task-tracker/app/consumers/accounts_stream_consumer.rb
+++ task-tracker/app/consumers/accounts_stream_consumer.rb
@@ -12,24 +12,36 @@ class AccountsStreamConsumer < ApplicationConsumer
         p payload
         puts '-' * 80
 
-        puts
-        case payload['event_name']
-        when 'AccountCreated'
-          Accounts::Create.call(payload['data'])
-        when 'AccountUpdated'
-          Accounts::Update.call(payload['data'])
-        when 'AccountDeleted'
-          Accounts::Delete.call(payload['data'])
-        when 'AccountRoleChanged'
-          Accounts::UpdateRoll.call(payload['data'])
+        result = SchemaRegistry.validate_event(payload, payload['event_type'], version: payload['event_version'])
+
+        if result.success?
+          process_acount_events(payload)
         else
-          # store events in DB
+          # TODO: save in the DB for later processing
+          puts "Event #{payload['event_type']} is invalid: #{result.errors}"
         end
       rescue
         puts '-' * 80
         p 'Failed to process the event'
         puts '-' * 80
       end
+    end
+  end
+
+  private
+
+  def process_acount_events(payload)
+    case payload['event_name']
+    when 'AccountCreated'
+      Accounts::Create.call(payload['data'])
+    when 'AccountUpdated'
+      Accounts::Update.call(payload['data'])
+    when 'AccountDeleted'
+      Accounts::Delete.call(payload['data'])
+    when 'AccountRoleChanged'
+      Accounts::UpdateRoll.call(payload['data'])
+    else
+      # store events in DB
     end
   end
 end
