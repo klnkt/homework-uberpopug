0eaf8baa6dfdcc82018c5b683d10db36675de9ba
Katja Efremova <kefremova@babbel.com>
2023-08-19 23:22:49 +0200

    Add JIRA ID to tasks

--- task-tracker/app/models/task.rb
+++ task-tracker/app/models/task.rb
@@ -4,11 +4,19 @@ class Task < ApplicationRecord
   scope :open, -> { where(status: 'New') }
   scope :completed, -> { where(status: 'Completed') }
 
+  validate :jira_id_not_in_title
+
   before_create :generate_uuid
 
   private
 
   def generate_uuid
     self.public_id = SecureRandom.uuid
+  end
+
+  def jira_id_not_in_title
+    return unless title.match?(/\[.*\]/)
+
+    errors.add(:title, 'cannot contain JIRA ID')
   end
 end
